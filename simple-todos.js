45cb9000ef14ba5a91da828d9e57fe9cdd3d4ab1
Scott Russell <scottedwardrussell@gmail.com>
2015-03-09 01:10:57 -0400

    Initial commit of simple-todos, before step 3 of tutorial

---
+++ simple-todos.js
@@ -0,0 +1,44 @@
+// simple-todos.js
+
+// create db connection
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+}
+
+//   Template.body.helpers({
+//     tasks: [
+//       { text: "This is task 1" },
+//       { text: "This is task 2" },
+//       { text: "This is task 3" }
+//     ]
+//   });
+// }
+
+//   Session.setDefault('counter', 0);
+
+//   Template.hello.helpers({
+//     counter: function () {
+//       return Session.get('counter');
+//     }
+//   });
+
+//   Template.hello.events({
+//     'click button': function () {
+//       // increment the counter when button is clicked
+//       Session.set('counter', Session.get('counter') + 1);
+//     }
+//   });
+// }
+
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+// }
