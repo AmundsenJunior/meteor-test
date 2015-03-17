c86977776a88f88b75c0a0e1dba7a6d90f4a06e9
Scott Russel <scottedwardrussell@gmail.com>
2015-03-17 12:09:54 -0400

    Added step 4 tutorial changes - new task function

--- simple-todos.js
+++ simple-todos.js
@@ -7,38 +7,26 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
     }
   });
 }
 
-//   Template.body.helpers({
-//     tasks: [
-//       { text: "This is task 1" },
-//       { text: "This is task 2" },
-//       { text: "This is task 3" }
-//     ]
-//   });
-// }
-
-//   Session.setDefault('counter', 0);
-
-//   Template.hello.helpers({
-//     counter: function () {
-//       return Session.get('counter');
-//     }
-//   });
-
-//   Template.hello.events({
-//     'click button': function () {
-//       // increment the counter when button is clicked
-//       Session.set('counter', Session.get('counter') + 1);
-//     }
-//   });
-// }
-
-// if (Meteor.isServer) {
-//   Meteor.startup(function () {
-//     // code to run on server at startup
-//   });
-// }
